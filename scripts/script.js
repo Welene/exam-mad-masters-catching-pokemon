68b29ac42a687b15c5449f0a505806b5663f71ec
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-02-04 09:36:05 +0100

    created variabels for createPokemons

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 const log = (msg) => console.log(msg);
 
-
-
 // Användaren startar vid ett formulär och ni skall formulärvalidera följande
 // --- Tränarens namn måste vara mellan 5 och 10 tecken långt
 // --- Tränaren måste vara mellan 10 och 15 år gammal
@@ -16,42 +14,48 @@ const log = (msg) => console.log(msg);
 
 //     oGameData.timeRef = document.querySelector(`#errorMsg`);
 //     oGameData.querySelector('#form');
-// }   
+// }
 
+log(oGameData.trainerAge);
 
 let errorMsg = document.createElement(`p`);
 errorMsg.id = `errorMsg`;
 errorMsg.style.color = `red`;
 
 const submitBtnRef = document.querySelector(`#submitBtn`);
-    submitBtnRef.insertAdjacentElement("afterend", errorMsg);
+submitBtnRef.insertAdjacentElement("afterend", errorMsg);
 
 function validateForm(event) {
-    oGameData.trainerName = document.querySelector (`#nick`).value;
-    oGameData.trainerAge = document.querySelector (`#age`).value;
-    log(oGameData.trainerName.length);
-    log(oGameData.trainerAge);
+  oGameData.trainerName = document.querySelector(`#nick`).value;
+  oGameData.trainerAge = document.querySelector(`#age`).value;
+  log(oGameData.trainerName.length);
+  log(oGameData.trainerAge);
 
-    try {
-    if(oGameData.trainerName.length < 5) {
-        throw new Error (`Your IRL name cannot possibly be that short... change name.`);
-        
+  try {
+    if (oGameData.trainerName.length < 5) {
+      throw new Error(
+        `Your IRL name cannot possibly be that short... change name.`
+      );
     } else if (oGameData.trainerName.length > 10) {
-        throw new Error (`Seriously? that name is way too long! Get a shorter one.`);
+      throw new Error(
+        `Seriously? that name is way too long! Get a shorter one.`
+      );
     } else {
-        log(errorMsg.textContent);
-        errorMsg.textContent = '';
+      log(errorMsg.textContent);
+      errorMsg.textContent = "";
     }
 
-
-    if(oGameData.trainerAge < 10) {
-        throw new Error (`You're too young for this... wait a few years and try again.`);
-        
+    if (oGameData.trainerAge < 10) {
+      throw new Error(
+        `You're too young for this... wait a few years and try again.`
+      );
     } else if (oGameData.trainerAge > 15) {
-        throw new Error (`You are way too old for Pokémon! (for legal reasons... that was a joke)`);
+      throw new Error(
+        `You are way too old for Pokémon! (for legal reasons... that was a joke)`
+      );
     } else {
-        log(errorMsg.textContent);
-        errorMsg.textContent = '';
+      log(errorMsg.textContent);
+      errorMsg.textContent = "";
     }
 
     const genderRadios = document.querySelectorAll(`input[name="gender"]`);
@@ -59,20 +63,28 @@ function validateForm(event) {
     let isSelected = false;
 
     for (let radio of genderRadios) {
-    if (radio.checked) {
+      if (radio.checked) {
         isSelected = true;
         break;
-        }
+      }
     }
     if (!isSelected) {
-    throw new Error(`Du måste välja pojke eller flicka`);
+      throw new Error(`Du måste välja pojke eller flicka`);
     }
     errorMsg.textContent = ``;
-} catch (error) {
+  } catch (error) {
     errorMsg.textContent = error.message;
     log(error.message);
     event.preventDefault();
-    }
+  }
 }
 
 document.querySelector(`#form`).addEventListener(`submit`, validateForm);
+
+const numPokemons = 10;
+const gameField = document.querySelector(`#gameField`);
+const pokemons = [];
+
+const pokeballImg = document.createElement(`img`);
+pokeballImg.src = `./assets/ball.webp`;
+pokeballImg.alt = `Pokemonboll`;
