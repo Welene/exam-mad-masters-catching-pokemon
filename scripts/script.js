3021d0dec11f3f3be1d5ef54a397184a073f8f8e
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-02-06 17:07:21 +0100

    changed randomImg

--- scripts/script.js
+++ scripts/script.js
@@ -113,8 +113,10 @@ function initiateGame() {
 
   document.body.style.backgroundImage = "url('../assets/arena-background.png')";
   oGameData.startTimeInMilliseconds();
+  let randomImg = randomPokemonImg(151, numPokemons);
+  log(randomImg);
   startMusic();
-  createPokemons();
+  createPokemons(randomImg);
   movePokemons();
 }
 
@@ -140,10 +142,7 @@ function randomPokemonImg(allImages, numImages) {
   return selectedImages;
 }
 
-let randomImg = randomPokemonImg(151, numPokemons);
-log(randomImg);
-
-function createPokemons() {
+function createPokemons(randomImg) {
   for (let i = 0; i < numPokemons; i++) {
     const pokemon = document.createElement(`img`);
     pokemon.dataset.id = randomImg[i];
